98747e862ae28e8ca5da102ed9b74f350e1562c6
AREUM\lee99 <lee990103@gmail.com>
2022-08-18 20:14:53 +0900

    공지사항 완성
    1. 공지사항 부분 화면 및 서버 구현 완성
    2. community,  trade vo 추가

--- semiTestPrj/src/com/kh/notice/controller/NoticeDetailController.java
+++ semiTestPrj/src/com/kh/notice/controller/NoticeDetailController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.notice.repository.NoticeDao;
 import com.kh.notice.service.NoticeService;
 import com.kh.notice.vo.NoticeVo;
 
@@ -21,22 +22,29 @@ public class NoticeDetailController extends HttpServlet {
 		// 우선, num번 게시글의 조회수 증가시키기
 		String num = req.getParameter("num");
 		
-//		int result = new NoticeService().increseNotice();
-//		
-//		if(result == 1) {
-//			
-//			//상세보기 화면 보여주기 -> 디비에서 특정 공지사항 정보 조회
-//			NoticeVo vo = 
-//			
-//			
-//			
-//		}else {
-//			//에러페이지
-//			
-//			
-//			
-//			
-//		}
+		int result = new NoticeService().increseNotice(num);
+		
+		if(result == 1) {
+			
+			//상세보기 화면 보여주기 -> 디비에서 특정 공지사항 정보 조회
+			NoticeVo vo = new NoticeService().selectOne(num);
+			
+			if(vo != null) {
+				//조회 성공 -> 정보를 담아서 포워딩 해주기!
+				req.setAttribute("vo", vo);
+				req.getRequestDispatcher("/views/notice/noticeDetail.jsp").forward(req, resp);
+				
+			} else {
+				//조회 실패 -> 에러페이지
+				req.setAttribute("alertMsg", "조회에 실패하셨습니다.");
+				req.getRequestDispatcher("/views/error/errorPage.jsp").forward(req, resp);
+			}
+			
+		} else {
+			//조회수 증가 실패 -> 에러페이지 
+			req.setAttribute("errorMsg", "조회에 실패하셨습니다.");
+			req.getRequestDispatcher("/views/error/errorPage.jsp").forward(req, resp);
+		}
 		
 		
 	}
--- semiTestPrj/src/com/kh/notice/service/NoticeService.java
+++ semiTestPrj/src/com/kh/notice/service/NoticeService.java
@@ -1,6 +1,7 @@
 package com.kh.notice.service;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import static com.kh.common.JDBCTemplate.*;
@@ -45,8 +46,8 @@ public class NoticeService {
 		int result = 0;
 		
 		//서비스 로직 작성하기
-		if(vo.getTitle().length() > 20) {
-			System.out.println("제목이 20글자를 초과하였습니다.");
+		if(vo.getTitle().length() > 30) {
+			System.out.println("제목이 30글자를 초과하였습니다.");
 			return -1;
 		}
 		
@@ -75,6 +76,131 @@ public class NoticeService {
 		}
 		
 		//결과 리턴
+		return result;
+	}
+
+
+	//공지사항 조회수 증가
+	public int increseNotice(String num) {
+		
+		//서비스 로직 작성
+		
+		
+		Connection conn = null;
+		int result = 0;
+		
+		try {
+			conn = getConnection();
+			// dao 호출
+			result = dao.increseNotice(conn, num);
+			
+			// 트랜잭션 처리
+			if(result == 1) {
+				commit(conn);
+			} else {
+				rollback(conn);
+			}
+			
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+				
+		// 결과 리턴
+		return result;
+	}
+
+
+	//공지사항 상세조회 하기
+	public NoticeVo selectOne(String num) {
+		
+		Connection conn = null;
+		NoticeVo vo = null;
+		
+		try {
+			//dao 호출하기
+			conn = getConnection();
+			vo = dao.selectOne(conn, num);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(conn);
+		}
+		
+		return vo;
+	}
+
+
+
+	//공지사항 삭제하기
+	public int delete(String num) {
+		
+		Connection conn = null;
+		int result = 0;
+		
+		try {
+			conn = getConnection();
+			
+			result = dao.delete(conn, num);
+			
+			if(result == 1) {
+				commit(conn);
+			} else {
+				rollback(conn);
+			}
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+			
+		} finally {
+			close(conn);
+		}
+		
+		return result;
+	}
+
+
+
+	//공지사항 수정하기
+	public int edit(NoticeVo vo) {
+		
+		
+		if(vo.getTitle().length() > 30) {
+			System.out.println("제목이 30글자를 초과하였습니다.");
+			return -1;
+		}
+		
+		if(vo.getContent().length() > 500) {
+			System.out.println("내용이 500글자를 초과하였습니다.");
+			return -2;
+		}
+		
+		
+		Connection conn = null;
+		int result = 0;
+		
+	
+		try {
+			conn = getConnection();
+			
+			result = dao.edit(conn, vo);
+			
+			if(result == 1) {
+				commit(conn);
+			} else {
+				rollback(conn);
+			}
+		} catch (Exception e) {
+			rollback(conn);
+			e.printStackTrace();
+			
+		} finally {
+			close(conn);
+		}
+		
 		return result;
 	}
 
